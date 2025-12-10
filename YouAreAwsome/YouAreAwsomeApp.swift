15e2b3e7aa0a348f3166d9f21aa1408d70d9410f
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2025-12-10 23:36:55 +0530

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Ethan on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
